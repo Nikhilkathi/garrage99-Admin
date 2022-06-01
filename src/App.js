436d1587b78c80b739bf19b3c0c7ef614b4d641a
Nikhilkathi <42873004+Nikhilkathi@users.noreply.github.com>
2022-06-01 14:44:12 +0530

    feat:(pages): Add Respective Pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import DashBoardA from "./Homescreen/DashBoardA";
+import NavigationBar from "./Navigation/NavigationBar";
 import CustomerPage from "./Homescreen/CustomerPage";
+import VendorPage from "./Homescreen/VendorPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
@@ -8,9 +10,11 @@ function App() {
   return (
     <>
       <BrowserRouter>
+        {window.location.pathname !== "/login" ? <NavigationBar /> : null}
         <Routes>
           <Route exact path="/" element={<DashBoardA />} />
           <Route exact path="/customer" element={<CustomerPage />} />
+          <Route exact path="/vendor" element={<VendorPage />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/Homescreen/VendorPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const VendorPage = () => {
+  return (
+    <main>
+      <div className="py-6">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+          <h1 className="ml-auto h-full">Hello</h1>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default VendorPage;
